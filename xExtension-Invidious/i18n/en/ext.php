4588a0c654c178f891c31727b687a6d08d1b6ffa
Tommy Miland <tmiland@gmail.com>
2019-01-26 15:39:06 +0100

    Update ext.php
    Added support for custom domain in settings.

--- xExtension-Invidious/i18n/en/ext.php
+++ xExtension-Invidious/i18n/en/ext.php
@@ -2,9 +2,10 @@
 
 return array(
 	'in_videos' => array(
+		'domain' => 'Player domain',
 		'height' => 'Player height',
-        'width' => 'Player width',
-        'updates' => 'You can find the latest extension version at',
+	  'width' => 'Player width',
+	  'updates' => 'You can find the latest extension version at',
 		'show_content' => 'Display the feeds content',
 	),
 );
